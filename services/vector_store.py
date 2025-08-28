0e735779a16012b6200b80a36d95f6b7c6ef3dba
Vishal <bcavishal14@gmail.com>
2025-08-28 09:22:06 +0530

    vector_store

--- services/vector_store.py
+++ services/vector_store.py
@@ -2,21 +2,22 @@ import chromadb
 
 client = chromadb.Client()
 collection = client.create_collection(name="documents")
-def add_data_to_database(File_name,embeding_list :list) ->list:
+def add_data_to_database(File_name, embeding_list: list) -> list:
     for item in embeding_list:
-      unique_id = item["id"]
-      original_text = item["text"]
-      embeding_text = item[["embedding"]]
+        unique_id = item["id"]
+        original_text = item["text"]
+        embeding_text = item["embedding"]
 
-      metadata = [{"File_Name":File_name}]
-      collection.add(
-      ids = unique_id,
-      documents=  original_text,
-      embeddings= embeding_text,
-      metadatas=metadata
-
-      
-      )
+        collection.add(
+            ids=[unique_id],                # ✅ list
+            documents=[original_text],      # ✅ list
+            embeddings=[embeding_text],     # ✅ already wrapped
+            metadatas=[{"File_Name": File_name}]
+        )
     
-    return collection.get(ids=["file_chunk_0"],include=["embeddings", "documents", "metadatas"])
+    return collection.get(
+        ids=["file_chunk_0"], 
+        include=["embeddings", "documents", "metadatas"]
+    )
+
     
